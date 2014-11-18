d9f23e9a701af76cd80f1f10f84e03a2a4b5d45c
Justin Yost <justin@loadsys.com>
2014-11-18 11:27:03 -0800

    Basic Deserailizing Implemented
    Signed-off-by: Justin Yost <justin@loadsys.com>

--- Routing/Filter/DeserializerFilter.php
+++ Routing/Filter/DeserializerFilter.php
@@ -1,25 +1,57 @@
 <?php
 /**
  *
- * @package  Serializers.Routing
+ * @package  Serializers.Routing.Filter
  */
 App::uses('DispatcherFilter', 'Routing');
+
+class DeserializerUnkownObject extends Exception {}
 
 /**
  *
  */
 class DeserializerFilter extends DispatcherFilter {
 
-	public $priority = 9;
-
+	/**
+	 * Method called before the controller is instantiated and called to serve a
+	 * request. If used with default priority, it will be called after the Router
+	 * has parsed the URL and set the routing params into the request object.
+	 *
+	 * We will process the request for the json data and de-serialize it
+	 *
+	 * @param  CakeEvent $event [description]
+	 * @return [type]           [description]
+	 */
 	public function beforeDispatch(CakeEvent $event) {
+		// get the request data
 		$request = $event->data['request'];
-		$response = $event->data['response'];
+		$controllerRequested = $request->params['controller'];
+		$data = $request->input('json_decode');
 
-		if ($request->url === 'hello-world') {
-			$response->body('Hello World');
-			$event->stopPropagation();
-			return $response;
+		if (!empty($data)) {
+			$data = $this->_deserializeData($data, $controllerRequested);
 		}
+
+		$request->data = $data;
+	}
+
+	/**
+	 * deseralize the response and turn it into something that cake expects
+	 *
+	 * @param  array  $data the serialized data
+	 * @return array
+	 */
+	protected function _deserializeData($serializedData = array(), $controllerRequested) {
+		$serializedObjectProperties = get_object_vars($serializedData);
+
+		if (!array_key_exists($controllerRequested, (array)$serializedObjectProperties)) {
+			$msg = "The controller name: $controllerRequested was not included in the passed in JSON body of the request.";
+			throw new DeserializerUnkownObject($msg);
+		}
+
+		$dataAsArray = get_object_vars($serializedData->{$controllerRequested});
+		$dataAsArray['ResinProduct'] = $dataAsArray;
+
+		return $dataAsArray;
 	}
 }
