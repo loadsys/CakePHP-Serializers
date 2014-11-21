58c1f6ca90d02d3739ee5d420d4c6f2e45d27c22
Justin Yost <justin@loadsys.com>
2014-11-20 18:38:48 -0800

    Docblock improvements
    Signed-off-by: Justin Yost <justin@loadsys.com>

--- Lib/SerializerNaming.php
+++ Lib/SerializerNaming.php
@@ -14,23 +14,25 @@ class SerializerNaming {
 	/**
 	 * the suffix used for naming a Serializer class
 	 *
-	 * @var String $suffix
+	 * @var string
 	 */
 	protected $suffix = 'Serializer';
 
 	/**
 	 * Convert a supplied string to a conventionally named Serializer class.
 	 *
-	 * @param String $name
-	 * @return String
+	 * @param  string $name the name for the class to generate
+	 * @return string       the name converted as needed
 	 */
 	public function classify($name = null) {
 		return Inflector::classify($this->stripSuffix($name)) . $this->suffix;
 	}
 
 	/**
-	 * @param String $name
-	 * @return String
+	 * Removes a suffix from a provided name
+	 *
+	 * @param  string $name the name for the class to generate
+	 * @return string       the name with a possible suffix stripped
 	 */
 	protected function stripSuffix($name) {
 		return preg_replace("/{$this->suffix}$/", '', $name);
