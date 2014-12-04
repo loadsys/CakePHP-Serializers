0ad13498f54696b0345b28a2bca2eabf1667b60c
Justin Yost <justin@loadsys.com>
2014-12-04 10:05:22 -0800

    Code Sniffer Updates
    Based upon update Loadsys Code Sniffer Rules
    
    Signed-off-by: Justin Yost <justin@loadsys.com>

--- Lib/AnalyzeRequest.php
+++ Lib/AnalyzeRequest.php
@@ -9,56 +9,58 @@ class AnalyzeRequest {
 	/**
 	 * the request being processed
 	 *
-	 * @var Object $_request
+	 * @var Object $request
 	 */
-	protected $_request;
+	protected $request;
 
 	/**
 	 * construct a new instance of the AnalyzeRequest class, set the passed in
 	 * request to the _request property
 	 *
-	 * @param CakeRequest $request
+	 * @param CakeRequest $request the CakeRequest object
 	 */
 	public function __construct($request) {
-		$this->_request = $request;
+		$this->request = $request;
 	}
 
 	/**
 	 * Checks various aspects of the request supplied on initialization
 	 * to determine if the request is JSON.
 	 *
-	 * @access public
-	 * @return Boolean
+	 * @return bool
 	 */
 	public function isJSON() {
 		return $this->contentType() || $this->accepts() || $this->extension();
 	}
 
 	/**
-	 * @access protected
-	 * @return Boolean
+	 * returns true only if the content type is application/json
+	 *
+	 * @return bool
 	 */
 	protected function contentType() {
-		return $this->_request->header('Content-Type') === 'application/json';
+		return $this->request->header('Content-Type') === 'application/json';
 	}
 
 	/**
-	 * @access protected
-	 * @return Boolean
+	 * returns true if the request accepts header, will accept json
+	 *
+	 * @return bool
 	 */
 	protected function accepts() {
-		$accepts = (array)$this->_request->accepts();
-		return array_reduce($accepts, function($prev, $i) {
+		$accepts = (array)$this->request->accepts();
+		return array_reduce($accepts, function ($prev, $i) {
 			return $prev ? $prev : preg_match('/^application\/.*json$/i', trim($i));
 		}, false);
 	}
 
 	/**
-	 * @access protected
-	 * @return Boolean
+	 * validates that the extension for the request is json
+	 *
+	 * @return bool
 	 */
 	protected function extension() {
-		$bits = explode('.', $this->_request->url);
+		$bits = explode('.', $this->request->url);
 		return $bits[count($bits) - 1] === 'json';
 	}
 }
