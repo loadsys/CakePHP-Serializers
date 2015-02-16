84a9f38a3194fb68d604a2c89e472d627a7d421f
Justin Yost <justin@loadsys.com>
2015-02-16 10:24:02 -0800

    Adds a new Custom EmberDataSerializerView Class with Test Class
    Signed-off-by: Justin Yost <justin@loadsys.com>

---
+++ Test/Case/View/EmberDataSerializerViewTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Class to the test the EmberDataSerializerView class
+ */
+App::uses('Controller', 'Controller');
+App::uses('CakeRequest', 'Network');
+App::uses('CakeResponse', 'Network');
+App::uses('EmberDataSerializer', 'Serializers.View');
+
+/**
+ * EmberDataSerializerViewTest
+ */
+class EmberDataSerializerViewTest extends CakeTestCase {
+
+	/**
+	 * setup the EmberDataSerializerView when testing
+	 *
+	 * @return void
+	 */
+	public function setUp() {
+		parent::setUp();
+		$this->EmberDataSerializerView = new EmberDataSerializerView();
+		$this->baseUrl = trim(Router::url('/', true), '/');
+	}
+
+	/**
+	 * tear down the CakeSerializerViewTest when testing
+	 *
+	 * @return void
+	 */
+	public function tearDown() {
+		unset($this->EmberDataSerializerView);
+		unset($this->baseUrl);
+		parent::tearDown();
+	}
+
+	/**
+	 * note that tests for this class are current incomplete
+	 *
+	 * @return void
+	 */
+	public function testIncomplete() {
+		$this->markTestIncomplete("EmberDataSerializerView currently has no test methods.");
+	}
+
+}
